5047c8f7c9aa8447f24b84428b693f926b225d63
Kevin <kevin.loiseleur@epitech.eu>
2017-06-21 16:19:50 +0200

    Fix conf.js exports

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
-module.exports = {
-    page_token : 'EAAcbFjDgYZBABAMAa9PH8aggwd2XBzPK11EmeZAH7azeDjIAyF9Xz23TYgZB1xBYakeib60vU5Ttd7AkoI8kWOiKzCyKTocXQzRKQswkgZCHaW9UpAx4Ue9HkICEkuH9USrxYJrceD01ypH3OySrCqtCJQUdKeHLaZBSJwCVYHAZDZD',
-    verif_token : 'this_is_my_mycanal-chatbot-token',
-    payload_greeting: 'Hello !'
-}
+
+exports.page_token = 'EAAcbFjDgYZBABAMAa9PH8aggwd2XBzPK11EmeZAH7azeDjIAyF9Xz23TYgZB1xBYakeib60vU5Ttd7AkoI8kWOiKzCyKTocXQzRKQswkgZCHaW9UpAx4Ue9HkICEkuH9USrxYJrceD01ypH3OySrCqtCJQUdKeHLaZBSJwCVYHAZDZD'
+exports.verif_token = 'this_is_my_mycanal-chatbot-token'
+exports.payload_greeting = 'Hello !'
+
